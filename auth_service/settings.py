c1b24e9223aa439af08712cd350f0732089798e6
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-08-31 14:03:11 -0400

    Fix: fixed CI to run Redis on docker

--- auth_service/settings.py
+++ auth_service/settings.py
@@ -148,15 +148,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL='users.User'
 
-#EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
-#DEFAULT_FROM_EMAIL='admin@admin.com'
-
-EMAIL_BACKEND=os.environ.get('EMAIL_BACKEND')
-EMAIL_HOST=os.environ.get('EMAIL_HOST')
-EMAIL_PORT=os.environ.get('EMAIL_PORT')
-EMAIL_USE_SSL=os.environ.get('EMAIL_USE_SSL')
-EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+if os.environ.get('CI') == 'true':
+    EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND=os.environ.get('EMAIL_BACKEND')
+    EMAIL_HOST=os.environ.get('EMAIL_HOST')
+    EMAIL_PORT=os.environ.get('EMAIL_PORT')
+    EMAIL_USE_SSL=os.environ.get('EMAIL_USE_SSL')
+    EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
 
 
